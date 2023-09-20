066622d0573f36e3e17373259a792feeef3462ef
saidlionsgeek <saidlionesgeek.1997@gmail.com>
2023-09-20 17:10:08 +0100

    auth

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,10 @@
 <?php
 
+use App\Http\Controllers\CartController;
+use App\Http\Controllers\ContactController;
+use App\Http\Controllers\HomeController;
 use App\Http\Controllers\ProfileController;
+use App\Http\Controllers\ShopController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -14,10 +18,10 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('frontend.pages.home');
-});
-
+Route::get('/', [HomeController::class , 'index'])->name('home.index');
+Route::get('/shop',[ShopController::class , 'index'])->name("shop.index");
+Route::get("/contact",[ContactController::class , 'index'])->name("contact.index");
+Route::get("/cart",[CartController::class , 'index'])->name("cart.index");
 Route::get('/dashboard', function () {
     return view('dashboard');
 })->middleware(['auth', 'verified'])->name('dashboard');
