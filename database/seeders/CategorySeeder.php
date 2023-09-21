8d06bacabea655f5249f44c4da34e9eb9f7e7fe7
saidlionsgeek <saidlionesgeek.1997@gmail.com>
2023-09-21 14:53:53 +0100

    catégor seeder

--- database/seeders/CategorySeeder.php
+++ database/seeders/CategorySeeder.php
@@ -2,6 +2,7 @@
 
 namespace Database\Seeders;
 
+use App\Models\category;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 
@@ -12,6 +13,10 @@ class CategorySeeder extends Seeder
      */
     public function run(): void
     {
-        //
+        category::insert([
+            ["name"=>"Office Chairs"],
+            ["name"=>"Club Chairs"],
+            ["name"=>"Rocking Chairs"],
+        ]);
     }
 }
